112b47a8f53149c18d938f26c37db11f59072231
wyq <489264018@qq.com>
2024-09-07 05:21:22 +0800

    update

--- Astar/src/engine/map.js
+++ Astar/src/engine/map.js
@@ -1,6 +1,10 @@
 class Map {
   constructor() {
     this.map = [];
+    this.startPoint = [];
+    this.endPoint = [];
+    this.AstarRoute = [];
+
   }
 
   initMap(width, height) {
@@ -18,6 +22,43 @@ class Map {
       let x = Math.floor(Math.random() * this.map.length);
       let y = Math.floor(Math.random() * this.map[0].length);
       this.map[x][y] = 1;
+    }
+  }
+
+  randomStartPoint() {
+    let x = Math.floor(Math.random() * this.map.length);
+    let y = Math.floor(Math.random() * this.map[0].length);
+    this.map[x][y] = 2;
+    this.startPoint = [x, y];
+    return [x, y];
+  }
+
+  randomEndPoint() {
+    let x = Math.floor(Math.random() * this.map.length);
+    let y = Math.floor(Math.random() * this.map[0].length);
+    this.map[x][y] = 3;
+    this.endPoint = [x, y];
+    return [x, y];
+  }
+
+  Astar() {
+    // A*算法
+    if(this.startPoint.length === 0 || this.endPoint.length === 0) {
+      return;
+    }
+    let openList = [];
+    let closeList = [];
+    let startPoint = {x: this.startPoint[0], y: this.startPoint[1], f: 0, g: 0, h: 0, parent: null};
+    openList.push(startPoint);
+    while(openList.length > 0) {
+      let current = openList[0];
+      for(let i=0; i<openList.length; i++) {
+        if(openList[i].f < current.f) {
+          current = openList[i];
+        }
+      }
+      openList.splice(openList.indexOf(current), 1);
+      closeList.push(current);
     }
   }
 }
